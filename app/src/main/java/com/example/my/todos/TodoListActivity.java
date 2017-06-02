524a4027eeb16943f952e3665d1c15b3c33576d5
MYounas <younus.ubit56@gmail.com>
2017-06-02 12:30:50 +0500

    stop

--- app/src/main/java/com/example/my/todos/TodoListActivity.java
+++ app/src/main/java/com/example/my/todos/TodoListActivity.java
@@ -1,5 +1,7 @@
 package com.example.my.todos;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +10,39 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TodoListActivity extends AppCompatActivity {
 
+    String[] itemname ={
+            "Get theatre tickets",
+            "Order pizza for tonight",
+            "Buy groceries",
+            "Running session at 19.30",
+            "Call Uncle Sam"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final ListView lv=(ListView) findViewById(R.id.lvTodos);
+//adds the custom layout
+        lv.setAdapter(new ArrayAdapter<String>(this, R.layout.todo_list_item,
+                R.id.tvNote,itemname));
+//adds the click event to the listView, reading the content
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                Intent intent = new Intent(TodoListActivity.this, TodoActivity.class);
+                String content= (String) lv.getItemAtPosition(pos);
+                intent.putExtra("Content", content);
+                startActivity(intent);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,27 +52,5 @@ public class TodoListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
